efbc7d85ae0709affb2fb53cbaba9197f288cff3
Patrick Wieth <patrick.wieth@googlemail.com>
2021-01-09 23:36:39 +0100

    fix bugs (targetable checkpoints, autoproduced+activatePrototype)

--- mods/sp/lua/tug_of_war.lua
+++ mods/sp/lua/tug_of_war.lua
@@ -124,7 +124,7 @@ TickTugOfWar = function()
 		captureStartPointActorA = Actor.Create("gdie1", true, { Owner = teamA[1], Location = startpointa.Location, SubCell = 1 })
 		captureStartPointActorB = Actor.Create("gdie1", true, { Owner = teamB[1], Location = startpointb.Location, SubCell = 1 })
 	elseif DateTime.GameTime == 2 then
-		captureStartPointActorA.Destroy()
+		captureStartPointActorA.Kill()
 		captureStartPointActorB.Destroy()
 	elseif DateTime.GameTime % 1000 > 0 then
 		-- we only do special tick things every 1000 Ticks, any tick else is skipped
